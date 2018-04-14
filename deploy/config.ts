cbb54d36070e4156c3ce09ba1e17deb7f40764e9
dviejopomata <davidviejopomata@gmail.com>
2018-04-14 03:59:00 +0200

    rm config.ts

--- deploy/config.ts
+++
@@ -1,5 +0,0 @@
-export const pullSecret = "registry";
-export const registryUrl = "docker.nextagilesoft.com";
-export const email = "davidviejopomata@gmail.com";
-export const user = "dviejo";
-export const pwd = "damehueco2012";
